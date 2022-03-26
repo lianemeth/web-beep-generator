ff2d741fb08559975a4058adc4d500a0b47b3f99
Lia Nemeth <liapnemeth@gmail.com>
2022-03-26 15:17:10 -0400

    play a nice cmajor chord

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import * as Tone from 'tone'
 
-function component() {
+function CMajor() {
     //create a synth and connect it to the main output (your speakers)
     const synth = new Tone.Synth().toDestination();
 
+    const now = Tone.now()
     //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n");
+    synth.triggerAttackRelease("C4", "8n", now);
+    synth.triggerAttackRelease("E4", "16n", now + 0.5);
+    synth.triggerAttackRelease("G4", "8n", now + 1);
 }
 
-document.body.appendChild(component());
+//attach a click listener to a play button
+document.querySelector('button')?.addEventListener('click', async () => {
+	await Tone.start();
+    CMajor();
+	console.log('audio is ready')
+})
+
+//document.body.appendChild(component());
