24bb1be8d70e2038720df6ce8fe508a17a2fcbbc
Lia Nemeth <liapnemeth@gmail.com>
2022-03-26 14:48:06 -0400

    produces a beep.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as Tone from 'tone'
+
+function component() {
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease("C4", "8n");
+}
+
+document.body.appendChild(component());
